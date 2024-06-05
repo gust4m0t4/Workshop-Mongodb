8b8280078c99fa9ce0d7473c48efc5fb79198b15
gust4m0t4 <gustavomotabr10@gmail.com>
2024-06-05 18:06:29 -0300

    first commit

---
+++ src/test/java/com/metatron/workshopmongo/WorkshopMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.metatron.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
