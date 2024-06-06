6eaac891ba1950c1089248f7367ddc9a8166cd1d
gust4m0t4 <gustavomotabr10@gmail.com>
2024-06-06 15:37:29 -0300

    find by id implements

--- src/main/java/com/metatron/workshopmongo/services/UserService.java
+++ src/main/java/com/metatron/workshopmongo/services/UserService.java
@@ -2,10 +2,12 @@ package com.metatron.workshopmongo.services;
 
 import com.metatron.workshopmongo.domain.User;
 import com.metatron.workshopmongo.repository.UserRepository;
+import com.metatron.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -15,6 +17,13 @@ public class UserService {
 
     public List<User> findAll() {
         return userRepository.findAll();
+
+    }
+
+    public User findById(String id) {
+        Optional<User> obj = userRepository.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("objeto não encontrado"));
+
 
     }
 }
