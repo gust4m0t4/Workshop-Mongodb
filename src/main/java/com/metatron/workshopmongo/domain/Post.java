0ef95c1c68608ee5209b6a6d3117879f6ef5a48a
gust4m0t4 <gustavomotabr10@gmail.com>
2024-06-08 17:58:31 -0300

    Implementing comments for posts

--- src/main/java/com/metatron/workshopmongo/domain/Post.java
+++ src/main/java/com/metatron/workshopmongo/domain/Post.java
@@ -1,11 +1,14 @@
 package com.metatron.workshopmongo.domain;
 
 import com.metatron.workshopmongo.dto.AuthorDTO;
+import com.metatron.workshopmongo.dto.CommentDTO;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 @Document
@@ -19,6 +22,8 @@ public class Post implements Serializable {
     private String body;
 
     private AuthorDTO author;
+
+    private List<CommentDTO> comments = new ArrayList<>();
 
     public Post() {
 
@@ -70,6 +75,13 @@ public class Post implements Serializable {
     public void setAuthor(AuthorDTO author) {
         this.author = author;
     }
+    public List<CommentDTO> getComments() {
+        return comments;
+    }
+
+    public void setComments(List<CommentDTO> comments) {
+        this.comments = comments;
+    }
 
     @Override
     public boolean equals(Object o) {
@@ -83,6 +95,7 @@ public class Post implements Serializable {
     public int hashCode() {
         return Objects.hashCode(id);
     }
+
 
 
 }
