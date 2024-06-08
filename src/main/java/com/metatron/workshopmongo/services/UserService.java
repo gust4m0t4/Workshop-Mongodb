6faeea0786a87a61e7f99429d4111aa398e520de
gust4m0t4 <gustavomotabr10@gmail.com>
2024-06-08 14:02:47 -0300

    Insert user and Delete user implements

--- src/main/java/com/metatron/workshopmongo/services/UserService.java
+++ src/main/java/com/metatron/workshopmongo/services/UserService.java
@@ -1,6 +1,7 @@
 package com.metatron.workshopmongo.services;
 
 import com.metatron.workshopmongo.domain.User;
+import com.metatron.workshopmongo.dto.UserDTO;
 import com.metatron.workshopmongo.repository.UserRepository;
 import com.metatron.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,7 +24,20 @@ public class UserService {
     public User findById(String id) {
         Optional<User> obj = userRepository.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("objeto não encontrado"));
-
+    }
+    public User insert(User obj) {
+        return userRepository.insert(obj);
 
     }
+    public void delete(String id) {
+        findById(id);
+        userRepository.deleteById(id);
+    }
+
+
+    public User fromDTO(UserDTO objDto) {
+        return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
+
+    }
+
 }
