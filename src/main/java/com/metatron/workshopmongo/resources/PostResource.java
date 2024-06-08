80d721def1783d323bd533a82f4661e448f4813a
gust4m0t4 <gustavomotabr10@gmail.com>
2024-06-08 19:26:05 -0300

    Multi query

--- src/main/java/com/metatron/workshopmongo/resources/PostResource.java
+++ src/main/java/com/metatron/workshopmongo/resources/PostResource.java
@@ -13,6 +13,7 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import com.metatron.workshopmongo.resources.util.URL;
 
 import java.net.URI;
+import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -33,6 +34,22 @@ public class PostResource {
 
         return ResponseEntity.ok().body(obj);
     }
+
+    @RequestMapping(value = "/fullsearch", method = RequestMethod.GET)
+    public ResponseEntity<List<Post>> fullSearch(
+            @RequestParam(value = "text", defaultValue = "") String text,
+            @RequestParam(value = "minDate", defaultValue = "") String minDate,
+            @RequestParam(value = "maxDate", defaultValue = "") String maxDate) {
+        text = URL.decodeParam(text);
+        Date min = URL.convertDate(minDate, new Date(0L));
+        Date max = URL.convertDate(maxDate, new Date());
+
+        List<Post> list = services.fullSearch(text, min, max);
+
+
+        return ResponseEntity.ok().body(list);
+    }
+
     @RequestMapping(value = "/titlesearch", method = RequestMethod.GET)
     public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text) {
         text = URL.decodeParam(text);
--- src/main/java/com/metatron/workshopmongo/services/PostService.java
+++ src/main/java/com/metatron/workshopmongo/services/PostService.java
@@ -8,6 +8,7 @@ import com.metatron.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -23,7 +24,14 @@ public class PostService {
         return obj.orElseThrow(() -> new ObjectNotFoundException("objeto não encontrado"));
     }
     public List<Post> findByTitle(String text) {
-        return postRepository.findByTitleContainingIgnoreCase(text);
+        return postRepository.searchTitle(text);
+    }
+    public List<Post> fullSearch(String text, Date minDate, Date maxDate) {
+        maxDate = new Date(maxDate.getTime() + 24 * 60 * 60 * 1000);
+        return postRepository.fullSearch(text, minDate, maxDate);
+
+
+
     }
 
 
